573038d8e52a08f4c93a538a31447d0f6e2045a2
DanyHudson <mail.d.hudson@gmail.com>
2025-09-09 22:06:54 +0200

    installed Angular, renamed files to filename.component.file ending, added landingpage component

---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule],
+    template: '<h1>Hallo Welt!</h1>',
+    styles: ['h1 {color: hotpink; text-align: center;}']
+})
+export class LandingPageComponent {
+
+}
