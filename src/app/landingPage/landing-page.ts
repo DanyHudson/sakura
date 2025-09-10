a1318b834bcc3370747a58590bb2ae8ffda25a62
DanyHudson <mail.d.hudson@gmail.com>
2025-09-10 22:49:08 +0200

    created and imported navbar

--- src/app/landingPage/landing-page.ts
+++ src/app/landingPage/landing-page.ts
@@ -1,20 +1,22 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Navbar } from "./navbar/navbar";
 
 @Component({
-    selector: 'app-landing-page',
-    standalone: true,
-    imports: [CommonModule],
-    template: `
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [CommonModule, Navbar],
+  template: `
     <section class="">
-      <h1>SAKURA RAMEN</h1>
-      <h2>BEST RAMEN IN TOWN</h2>
+    <app-navbar></app-navbar>
+      <h1 class="font-Raleway">SAKURA RAMEN</h1>
+      <h2 class="font-Raleway">BEST RAMEN IN TOWN</h2>
     </section>
     
     `,
 
 
-    styleUrls: ['./landing-page.scss']
+  styleUrls: ['./landing-page.scss']
 })
 export class LandingPage {
 
