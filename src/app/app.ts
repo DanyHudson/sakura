70c64ee0cbb7f68815f716aeda3ce5b0a0630914
DanyHudson <mail.d.hudson@gmail.com>
2025-09-16 16:27:45 +0200

    adjusted links, added routing and adjusted referencing of components accordingly

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MainContent } from './main-content/main-content';
 import { Footer } from './shared/components/footer/footer';
-import { Imprint } from './imprint/imprint';
+
 
 @Component({
   selector: 'app-root',
   imports: [
     CommonModule,
     RouterOutlet,
-    MainContent,
-    Imprint,
     Footer
   ],
   templateUrl: './app.html',
